72bc5a47dba74d73722422254a465da94c890077
Alyeldin22 <alyeldin885311@gmail.com>
2025-07-30 19:11:17 +0300

    testtyy

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5178,
-    proxy: {
-      '/api': {
-        target: 'https://booking-app-db.vercel.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: true,
-      }
-    }
-  }
-})
+  base: '/Bookler/',
+});
